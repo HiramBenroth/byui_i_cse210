b1539003e89ff46aec2a2b35e076d7bc78b1df10
hrodb <hiram.benroth@gmail.com>
2024-11-02 16:49:50 -0600

    This the james bond name Program

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.Versioning;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string fName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lName = Console.ReadLine();
+
+        Console.Write($"\n\nYour name is {lName}, {fName} {lName}");
+
     }
 }
