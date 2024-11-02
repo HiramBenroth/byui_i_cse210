e237099d50bac3eecb4ca8c54ed12c67c631a553
hrodb <hiram.benroth@gmail.com>
2024-11-02 17:09:44 -0600

    Number Grade Code block

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the number grade? ");
+        int grade = int.Parse(Console.ReadLine());
+        string lGrade = "F";
+        if (grade >= 90) {
+            lGrade = "A";
+        }
+        else if(grade >= 80) {
+            lGrade = "B";
+        }
+        else if(grade >= 70){
+            lGrade = "C";
+        }
+        else if(grade >= 60){
+            lGrade = "D";
+        }
+
+        Console.WriteLine($"\nYour letter grade is a {lGrade}");
+
     }
 }
